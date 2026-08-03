be376e4777b20b98f43d42744d5f05b0c0d80cf6
Jaemini <kimjaemin0870@gmail.com>
2026-08-03 10:57:49 +0900

    [Passed] 합과 평균 (1ms, 0MB)

---
+++ trail0/합과 평균/sum-and-avg.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    double A;
+    double B;
+    
+    scanf("%lf", &A);
+    scanf("%lf", &B);
+
+    printf("%.0lf %.1lf\n", A + B, ( A + B ) / 2);
+
+    return 0;
+    // Please write your code here.
+    return 0;
+}
